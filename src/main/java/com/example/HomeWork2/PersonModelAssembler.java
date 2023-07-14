ab390eea4a2514beade3f462a18e4cc0433ec320
Vadims Gostevs <vadims.gostevs@gmail.com>
2023-07-14 16:12:03 +0300

    Added @Nonnull annotations to PersonModelAssembler.java, deleted unused imports from PersonNotFoundAdvice.java.

--- src/main/java/com/example/HomeWork2/PersonModelAssembler.java
+++ src/main/java/com/example/HomeWork2/PersonModelAssembler.java
@@ -2,6 +2,7 @@ package com.example.HomeWork2;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -10,7 +11,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @Component
 public class PersonModelAssembler implements RepresentationModelAssembler<Person, EntityModel<Person>> {
     @Override
-    public EntityModel<Person> toModel(Person person) {
+    @NonNull
+    public EntityModel<Person> toModel(@NonNull Person person) {
         return EntityModel.of(person,
                 linkTo(methodOn(PersonController.class).one(person.getId())).withSelfRel(),
                 linkTo(methodOn(PersonController.class).getAll()).withRel("persons"));
--- src/main/java/com/example/HomeWork2/PersonNotFoundAdvice.java
+++ src/main/java/com/example/HomeWork2/PersonNotFoundAdvice.java
@@ -1,15 +1,10 @@
 package com.example.HomeWork2;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class PersonNotFoundAdvice {
